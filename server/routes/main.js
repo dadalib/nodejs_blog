9522c077090a1b9e18b4639f4d97fc81865a2c06
darwin <darwin.libioulle@gmail.com>
2024-06-18 21:07:19 +0200

    Add link to body

--- server/routes/main.js
+++ server/routes/main.js
@@ -71,6 +71,28 @@ router.get('', async (req, res) => {
 
 /**
  * GET /
+ * Post : id
+*/
+
+router.get('/post/:id', async(req, res) => {
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+    let slug = req.params.id;
+
+    try{
+        const data = await Post.findById({_id: slug});
+        res.render('post',{locals,data});
+    }catch(error){
+        console.log(error);
+    }
+
+    // res.render('index', { locals });
+});
+
+/**
+ * GET /
  * About
 */
 router.get('/about', (req, res) => {
